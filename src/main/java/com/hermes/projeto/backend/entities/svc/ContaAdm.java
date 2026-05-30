017e7adf6fd3c7e10ddf611b3a84aa28f0a92fd9
SilvaEng7 <rianalemeida11@gmail.com>
2026-05-30 17:07:57 -0300

    Criação final das entidades

--- src/main/java/com/hermes/projeto/backend/entities/svc/ContaAdm.java
+++ src/main/java/com/hermes/projeto/backend/entities/svc/ContaAdm.java
@@ -1,3 +1,79 @@
 package com.hermes.projeto.backend.entities.svc;
 
 
+import com.hermes.projeto.backend.dto.DadosLoginDTO;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity(name = "ContaAdm")
+@Table(name = "conta_adm")
+public class ContaAdm implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idConta")
+    private Long idContaAdm;
+
+    @Column(name = "nome_conta", nullable = false)
+    private String nomeConta;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String senha;
+
+    private Boolean ativo;
+
+   
+    public ContaAdm(DadosLoginDTO dados, String senhaCriptografada) {
+        this.username = dados.username();
+        this.senha = senhaCriptografada;
+    }
+
+    public ContaAdm() {
+
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}

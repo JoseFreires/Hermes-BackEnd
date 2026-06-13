2018ed96a2b2de9179b367363e43262974118e1d
Caio Henrique <caiohamoreira@gmail.com>
2026-06-13 17:53:52 -0300

    Implementando lógica de segurança CORS
    Adicionando método para possibilitar a realização de requisições do front-end e o CORS não barrar.

--- src/main/java/com/hermes/projeto/backend/entities/security/config/SecurityConfig.java
+++ src/main/java/com/hermes/projeto/backend/entities/security/config/SecurityConfig.java
@@ -5,14 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;    
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -25,12 +33,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
+        return http.csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(request -> {
                 // Autenticação - permitir acesso público
                 request.requestMatchers(HttpMethod.POST, "/login").permitAll();
-                request.requestMatchers(HttpMethod.POST, "/login/eu").permitAll();
+                request.requestMatchers(HttpMethod.GET, "/login/eu").permitAll();
 
                 // ENCOMENDAS
                 request.requestMatchers(HttpMethod.POST, "/encomendas").hasAnyRole("PORTEIRO", "ADMIN");
@@ -77,9 +85,29 @@ public class SecurityConfig {
                 request.anyRequest().authenticated();
 
             })
-        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).cors(Customizer.withDefaults())
         .build();
 }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // porta onde o Front-end está rodando
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+
+        // Relação do cookie
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        // Aplica a regra do CORS para todos os endpoints API
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
